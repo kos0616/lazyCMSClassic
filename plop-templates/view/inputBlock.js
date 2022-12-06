069da0d8630a959571b68b92b20a7c50e91d53ac
Mark <Mark@b.com>
2022-12-06 13:38:05 +0800

    restore others

--- plop-templates/view/inputBlock.js
+++ plop-templates/view/inputBlock.js
@@ -16,19 +16,19 @@ module.exports =
 {{else if this.spec.options.length}}
 <el-select
   v-model="form[inputForm.{{this.key}}.id]"
-  :placeholder="$t(inputForm.{{this.key}}.label)"
+  :placeholder="$i18n.global.t(inputForm.{{this.key}}.label)"
 >
   <el-option
     v-for="(item, i) in inputForm.{{this.key}}.options"
     :key="\`{{this.key}}_\${i}\`"
-    :label="$t(item.label)"
+    :label="$i18n.global.t(item.label)"
     :value="item.value"
   />
 </el-select>
 {{else}}
 <el-input
   v-model="form[inputForm.{{this.key}}.id]"
-  :placeholder="$t(inputForm.{{this.key}}.label)"
+  :placeholder="$i18n.global.t(inputForm.{{this.key}}.label)"
   {{#if this.spec.memo}}
   type="textarea"
   {{/if}}
